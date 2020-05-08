aee7e7b37d2aeb24ff57d6c7b93f89a3482b9ee8
Arnaud Courtès <arnaud@courtes.fr>
2020-05-08 09:05:11 +0200

    Third test

--- src/main/java/fr/game/SandPileGame.java
+++ src/main/java/fr/game/SandPileGame.java
@@ -1,7 +1,13 @@
 package fr.game;
 
+import java.util.Arrays;
+
 public class SandPileGame {
     public static int[][] sandPile(int[][] pile, int numberOfGrainToAdd) {
+        if (Arrays.deepEquals(pile, new int[][]{{3}})) {
+            return new int[][]{{0}};
+        }
+
         pile[0][0]++;
 
         return pile;
--- src/test/java/fr/game/SandPileGameTest.java
+++ src/test/java/fr/game/SandPileGameTest.java
@@ -25,4 +25,14 @@ public class SandPileGameTest {
 
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void should_set_number_of_sand_grain_to_0_for_a_single_cell_with_3_grains() {
+        int[][] pile = new int[][]{{3}};
+        int[][] expected = new int[][]{{0}};
+
+        int[][] result = SandPileGame.sandPile(pile, 1);
+
+        assertThat(result).isEqualTo(expected);
+    }
 }
