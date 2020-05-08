bd46526aeb6d091deda6fed83b00843406c80ad8
Arnaud Courtès <arnaud@courtes.fr>
2020-05-08 09:11:02 +0200

    Refactoring

--- src/main/java/fr/game/SandPileGame.java
+++ src/main/java/fr/game/SandPileGame.java
@@ -5,11 +5,9 @@ public class SandPileGame {
     public static final int MAXIMUM_NUMBER_OF_SAND_GRAIN = 4;
 
     public static int[][] sandPile(int[][] pile, int numberOfGrainToAdd) {
-        if (numberOfGrainToAdd == 2 || numberOfGrainToAdd == 3) {
-            return new int[][]{{3}};
+        for (int i = 0; i < numberOfGrainToAdd; i++) {
+            pile[0][0]++;
         }
-
-        pile[0][0]++;
 
         if (pile[0][0] == MAXIMUM_NUMBER_OF_SAND_GRAIN) {
             pile[0][0] = 0;
