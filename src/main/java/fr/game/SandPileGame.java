507e10371efff78a09b6911051ab7faaf70ab0f1
Arnaud Courtès <arnaud@courtes.fr>
2020-05-08 09:07:06 +0200

    Refactoring

--- src/main/java/fr/game/SandPileGame.java
+++ src/main/java/fr/game/SandPileGame.java
@@ -1,14 +1,15 @@
 package fr.game;
 
-import java.util.Arrays;
-
 public class SandPileGame {
-    public static int[][] sandPile(int[][] pile, int numberOfGrainToAdd) {
-        if (Arrays.deepEquals(pile, new int[][]{{3}})) {
-            return new int[][]{{0}};
-        }
 
+    public static final int MAXIMUM_NUMBER_OF_SAND_GRAIN = 4;
+
+    public static int[][] sandPile(int[][] pile, int numberOfGrainToAdd) {
         pile[0][0]++;
+
+        if (pile[0][0] == MAXIMUM_NUMBER_OF_SAND_GRAIN) {
+            pile[0][0] = 0;
+        }
 
         return pile;
     }
